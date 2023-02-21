d58f77d6401c317dbed8a1603bda7a7a04a5ceef
Leonardo Vieira <leonardo.vieira@br.experian.com>
2023-02-21 12:38:40 -0300

    created horizontal listview

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:listview/square.dart';
+import 'package:listview/circle.dart';
 
 class Homepage extends StatelessWidget {
   // const Homepage({super.key});
-  final List _posts = [
-    'post_1',
-    'post_2',
-    'post_3',
+  final List _stories = [
+    'story 1',
+    'story 2',
+    'story 3',
+    'story 4',
+    'story 5',
+    'story 6',
   ];
+
+  final List _posts = ['post 1', 'post 2', 'post 3', 'post 4'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
+          // instagram stories
           Container(
-            height: 300,
+            height: 150,
+            child: ListView.builder(
+                itemCount: _stories.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Circle(
+                    child: _stories[index],
+                  );
+                }),
+          ),
+
+          // instagram posts
+          Expanded(
             child: ListView.builder(
               itemCount: _posts.length,
               itemBuilder: (context, index) {
